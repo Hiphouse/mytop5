eba1628f24daf15169869591383835dfacd5440e
Nathan <the.nathan.lovell@gmail.com>
2022-11-01 13:44:17 -0700

    routing from homepage complete, first selection page wip

---
+++ pages/components/Selections.tsx
@@ -0,0 +1,30 @@
+import styles from '../../styles/Home.module.css'
+
+
+
+
+export default function Selections(props: any):JSX.Element {
+    
+    const selections = ['movies', 'shows', 'songs', 'artists', 'books'];
+    
+    const Choices = selections.map((selection, index) => {
+      const name = styles[selection]
+      const nameSelected = styles[selection+'Selected']
+      return (<button key={index} id={selection} className={!props.selectedCategories[selection as keyof typeof props.selectedCategories] ? `${name}` : `${nameSelected}`} onClick={props.handleCategoryClick}>{selection}</button>
+    )})
+    
+    return <>{
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to MyTop5!
+        </h1>
+
+        <p className={styles.description}>
+          Select from the categories below to generate your top 5
+        </p>
+        <div className={styles.categoryGrid}>
+          {Choices}
+        </div>
+      </main>
+    }</>
+  }
--- pages/select/movies.tsx
+++ pages/select/movies.tsx
@@ -1,3 +1,37 @@
-export default function MoviesSelect() {
-  
+import { useRouter } from 'next/router';
+import { FormEventHandler, useEffect, useState } from 'react';
+
+export default function MoviesSelect(props: any) {
+    const [formInputData, setFormInputData] = useState('');
+    useEffect(() => {
+        const savedCategories = localStorage.getItem("categories")
+        let categories: string[] = [];
+        if (typeof savedCategories === 'string') categories = JSON.parse(savedCategories);
+        console.log(categories)
+        
+        categories = categories.slice(1, categories.length)
+        console.log('categories after removing first idx: ', categories);
+        window.localStorage.setItem('categories', JSON.stringify(categories))
+        
+
+    }, [])
+    const router = useRouter()
+
+    const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const target = e.target as HTMLFormElement;
+        console.log(target.value)
+        setFormInputData(target.value)
+    }
+   
+    
+  return(
+    <>
+    <h1>Movies</h1>
+    <form onChange={handleFormSubmit}>
+        <label htmlFor="movies">Search</label>
+        <input id='movies' type="text" placeholder="Type your movie here..." value={formInputData}/>
+    </form>
+    </>
+  )
 }
